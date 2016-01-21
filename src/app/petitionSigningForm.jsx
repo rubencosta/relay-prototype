8ee352191ec589d77614dfba455bf14d4586e22d
rubencosta <rmc@nubolab.org>
2016-01-21 19:05:07 +0000

    form: trigger mutation with applyUpdate and react to success/failure

--- src/app/petitionSigningForm.jsx
+++ src/app/petitionSigningForm.jsx
@@ -6,28 +6,42 @@ class PetitionSigningForm extends Component {
   onSubmit(event) {
     event.preventDefault()
     const {name: {value: name}, email: {value: email}} = event.target.elements
-    Store.commitUpdate(new SignPetitionMutation({
+    Store.applyUpdate(new SignPetitionMutation({
       petition: this.props.petition,
       name,
       email
-    }))
+    }), {onFailure: ::this.onFailure, onSuccess: ::this.onSuccess}).commit()
+  }
+
+  onSuccess() {
+    this.reset()
+  }
+
+  onFailure(transaction) {
+    const error = transaction.getError() || new Error('Mutation failed.')
+    console.error(error)
+  }
+
+  reset() {
+    this.refs.form.reset()
   }
 
   render() {
     return (
       <section>
-        <form onSubmit={::this.onSubmit}>
+        <form ref="form" onSubmit={::this.onSubmit}>
           <label htmlFor="name">name:</label>
           <input
             type="text"
             name="name"
-            required />
+            required/>
           <label htmlFor="email">email:</label>
           <input
             type="email"
             name="email"
-            required />
+            required/>
           <button type="submit">Sign</button>
+          <button onClick={::this.reset}>reset</button>
         </form>
       </section>
     )
